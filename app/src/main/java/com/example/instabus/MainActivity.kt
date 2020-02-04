8c877179fc378a5a9bb08259333ab5041a5aab1d
Damien FONTAINE <279530@supinfo.com>
2020-02-04 15:43:30 +0100

    v11

--- app/src/main/java/com/example/instabus/MainActivity.kt
+++ app/src/main/java/com/example/instabus/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a = "hell"
+        val a = "hell7777777"
     }
 }
